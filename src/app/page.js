689ca5751142ce3299da0486478f50f66536d233
clarklindev <clarklindev@gmail.com>
2025-06-12 21:16:04 +0800

    basic example

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,20 @@
-import Image from "next/image";
+"use client";
+
 import styles from "./page.module.css";
+import { useStore } from "@/store/store";
 
 export default function Home() {
+  const bears = useStore((state) => state.bears);
+
   return (
     <div className={styles.page}>
-      <main className={styles.main}>helloworld</main>
+      <main className={styles.main}>
+        <h1>{bears} bears around here...</h1>
+        <button onClick={useStore((state) => state.increasePopulation)}>
+          one up
+        </button>
+        <p>{}</p>
+      </main>
     </div>
   );
 }
