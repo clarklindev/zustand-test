806badd8d43fa4e947b762d4af108108d8cd29da
clarklindev <clarklindev@gmail.com>
2025-06-12 21:22:40 +0800

    increasePopulation assigned to useStore directly

--- src/app/page.js
+++ src/app/page.js
@@ -5,14 +5,12 @@ import { useStore } from "@/store/store";
 
 export default function Home() {
   const bears = useStore((state) => state.bears);
-
+  const increasePopulation = useStore((state) => state.increasePopulation);
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <h1>{bears} bears around here...</h1>
-        <button onClick={useStore((state) => state.increasePopulation)}>
-          one up
-        </button>
+        <button onClick={increasePopulation}>one up</button>
         <p>{}</p>
       </main>
     </div>
